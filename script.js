d7c1d150bdadc91ebefb4c6acaa7b530f5c73ccc
Hamil <dimapanat.hamil@gmail.com>
2023-05-08 19:37:46 -0400

    blobbity'

---
+++ script.js
@@ -0,0 +1,24 @@
+const options = {
+    bottom: '64px', // default: '32px'
+    right: '32px', // default: '32px'
+    left: 'unset', // default: 'unset'
+    time: '0.5s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#F6E1C3',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: false, // default: true,
+    label: '🌓', // default: ''
+    autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
+
+
+const option2 = {
+    color: '#E9A178',
+    dotColor: 'white',
+    invert: true
+};
+new Blobity(option2);
